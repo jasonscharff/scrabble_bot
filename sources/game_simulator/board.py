484b24a3bef159e1ae097034782244f51ff154a9
Jason Scharff <jasonscharff@gmail.com>
2020-10-19 18:45:07 -0700

    Add board and tiles

---
+++ sources/game_simulator/board.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+class Board:
+
+    def __init__(self):
+        self.board = self.__initialize_board()
+
+    def __initialize_board(self):
+        board = []
+        with open('../static/board.txt', 'r') as f:
+            for line in f:
+                board.append(list(map(lambda x: Board.Space(x), line.strip('\n').split(' '))))
+        return board
+
+    def __str__(self):
+        all_rows = []
+        if len(self.board) > 0:
+            header_row = ' ' * 4 + ' '.join([str(i).zfill(3) for i in range(len(self.board[0]))])
+            all_rows.append(header_row)
+        for i, row in enumerate(self.board):
+            row_representation = []
+            for s in row:
+                if s.letter is not None:
+                    row_representation.append(' ' + s.letter + ' ')
+                else:
+                    row_representation.append(s.space_type)
+            all_rows.append('{} {}'.format(str(i).zfill(3), ' '.join(row_representation)))
+        return '\n'.join(all_rows)
+
+
+    def is_valid_play(self, word, starting_coordinate, direction)
+        pass
+
+    def play(self, word, starting_coordinate, direction):
+        pass
+
+
+
+
+    class Direction(Enum):
+        HORIZONTAL = 0
+        VERTICAL = 1
+
+    class Space:
+
+        class SpaceType(Enum):
+            REGULAR = 'RES'
+            DOUBLE_LETTER = '2LS'
+            TRIPLE_LETTER = '3LS'
+            DOUBLE_WORD = '2WS'
+            TRIPLE_WORD = '3WS'
+
+        def __init__(self, space_type=SpaceType.REGULAR, letter=None):
+            self.space_type = space_type
+            self.letter = letter
+
+
+if __name__ == '__main__':
+    b = Board()
+    print(b)
---
+++ sources/game_simulator/tile_bag.py
@@ -0,0 +1,31 @@
+import random
+
+
+class Tile:
+
+    def __init__(self, letter, point_value):
+        self.letter = letter
+        self.point_value = point_value
+
+    def __str__(self):
+        return self.letter
+
+
+class TileBag:
+
+    def __init__(self):
+        self.tiles = self._generate_tiles()
+
+    def draw(self, number):
+        return self.tiles.pop(number)
+
+    def __generate_tiles(self):
+        all_tiles = []
+        with open('../static/board.txt', 'r') as f:
+            for line in f:
+                count, letter, value = line.strip('\n').strip(' ')
+                all_tiles += [Tile(letter, int(value)) for _ in range(int(count))]
+
+        random.shuffle(all_tiles)
+        return all_tiles
+
